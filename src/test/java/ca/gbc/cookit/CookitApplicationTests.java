3835c0564d289822a09bc73f3561825b65f57d85
raufdevo <raufdevo@gmail.com>
2023-09-10 20:48:02 -0400

    First Commit

---
+++ src/test/java/ca/gbc/cookit/CookitApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.cookit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
